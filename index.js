b473038dfdcbc2b1826476e1e5c3c68c495e4532
MH-Kniloy <web.kniloy@gmail.com>
2025-02-27 03:49:32 +0600

    mongodb connected through mongoose

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 3000;
+
+mongoose
+  .connect(
+    "mongodb+srv://kniloy:16051995bd@cluster0.vdl71.mongodb.net/userInfo?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected!"));
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
