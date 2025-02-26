3b1bd309a99ae9f28ee75adc46e43c9898e29f3c
MH-Kniloy <web.kniloy@gmail.com>
2025-02-27 04:44:09 +0600

    data sent to database

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const UserList = require("./schema/userSchema")
 const port = 3000;
 
+app.use(express.json())
 mongoose
   .connect(
     "mongodb+srv://kniloy:16051995bd@cluster0.vdl71.mongodb.net/userInfo?retryWrites=true&w=majority&appName=Cluster0"
@@ -11,9 +12,48 @@ mongoose
   .then(() => console.log("Connected!"));
 
 
+const users = [
+  {
+    firstname: "niloy",
+    lastname: "hassan",
+    email: "niloyhassan@gmail.com",
+    age: 25,
+  },
+  {
+    firstname: "niloy",
+    lastname: "hassan2",
+    email: "niloyhassan2@gmail.com",
+    age: 35,
+  },
+  {
+    firstname: "niloy",
+    lastname: "hassan3",
+    email: "niloyhassan3@gmail.com",
+    age: 75,
+  },
+  {
+    firstname: "niloy",
+    lastname: "hassan4",
+    email: "niloyhassan4@gmail.com",
+    age: 55,
+  },
+]; 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(users);
 });
+
+app.post("/users", (req, res) =>{
+    console.log(req.body)
+    const {fname,lname,email,age} = req.body
+
+    const users = new UserList({
+        firstname:fname,
+        lastname:lname,
+        email:email,
+        age:age
+    })
+    users.save()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
